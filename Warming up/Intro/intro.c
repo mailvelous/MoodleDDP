4c0746b969d74563581f37ff35daadf47faccf71
mailvelous <mailvelous0@gmail.com>
2024-11-12 08:28:10 +0700

    feat: edit warmingup/intro

---
+++ Warming up/Intro/intro.c
@@ -0,0 +1,21 @@
+/* 
+Deskripsi : menampilkan bilangan dua kali lipat
+Nama /Author : Fauzi Ismail
+Tanggal/versi :
+*/
+
+#include<stdio.h>
+
+int main(){
+    //tulis jawaban anda dibawah ini
+    int X;
+    int Y;
+    int result;
+
+    scanf("%d", &X);
+    scanf("%d", &Y);
+    result = X + Y;
+    printf("%d", result);
+    
+    return 0;
+}

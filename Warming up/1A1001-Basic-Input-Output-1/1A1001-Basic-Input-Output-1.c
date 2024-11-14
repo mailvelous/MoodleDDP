ee7f17f3254707db37b7b28c136cc585cf788ed2
mailvelous <mailvelous0@gmail.com>
2024-11-14 11:38:00 +0700

    feat: add 1A1001-Basic-Input-Output

---
+++ Warming up/1A1001-Basic-Input-Output-1/1A1001-Basic-Input-Output-1.c
@@ -0,0 +1,28 @@
+/* 
+Deskripsi : menampilkan bilangan dua kali lipat
+Nama /Author :
+Tanggal/versi :
+*/
+
+/* 
+Deskripsi : menampilkan bilangan dua kali lipat
+Nama /Author : Fauzi Ismail
+Tanggal/versi : 21/10/2024
+*/
+
+#include<stdio.h>
+
+int main(){
+    //tulis jawaban anda dibawah ini
+    int X;
+    int result;
+
+    scanf("%d", &X);
+    result = X*2;
+    printf("%d", result);
+    
+    return 0;
+}
+
+
+
